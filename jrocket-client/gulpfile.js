238d0f600481a90b591329f4167d96e069a10a60
Pawel Mackowski <pawel.piotr.mackowski@gmail.com>
2015-12-09 20:21:57 +0100

    [client] es6-shim support added

--- jrocket-client/gulpfile.js
+++ jrocket-client/gulpfile.js
@@ -30,6 +30,7 @@ gulp.task('css', function () {
 
 gulp.task('lib', function () {
     return gulp.src([
+        'node_modules/es6-shim/es6-shim.js',
         'node_modules/systemjs/dist/system.js',
         'node_modules/angular2/bundles/angular2.dev.js'])
         .pipe(gulp.dest(DIST + 'lib'));
